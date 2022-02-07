bc8606b8aaba4aa9d9e38a4fa2b8bf39ecb2e5a6
Allan Machado <allan_asmachado@hotmail.com>
2022-02-07 17:39:30 -0300

    updates

--- aula19/ext/config/__init__.py
+++ aula19/ext/config/__init__.py
@@ -1,6 +1,6 @@
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://gersonrs:@8T97VKcaqp!!Ff@gersonrs.mysql.pythonanywhere-services.com/gersonrs$default'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://allmach1:FOOT035a#@allmach1.mysql.pythonanywhere-services.com/allmach1$default'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['SECRET_KEY'] = "asdasdsadasd"
     app.config['JWT_SECRET_KEY'] = 'asfasfasfas'
